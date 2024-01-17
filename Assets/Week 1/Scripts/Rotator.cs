d0e941ee02220b77a7c6cf3f12a856bd08ce509b
DESKTOP-JBLOCP0\ethan <ethancarreon621@gmail.com>
2024-01-16 20:51:16 -0500

    Week1Task1: clock with rotating hands completed

---
+++ Assets/Week 1/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Rotator : MonoBehaviour
+{
+    public float speed = 360;
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, -speed * Time.deltaTime);
+    }
+}
