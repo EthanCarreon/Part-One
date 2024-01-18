e035844bb1adfe653f3db50bc9e15799dd5a4ad8
carreoet <carreoet@OA-S141-15>
2024-01-18 11:07:26 -0500

    Week2-Task2 completed

---
+++ Assets/Week 2/Scripts/Switch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.green;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger from: " + collision.gameObject);
+        spriteRenderer.color = Color.red;
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteRenderer.color = Color.green;
+    }
+}
